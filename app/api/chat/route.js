11d3220e3d3eedf8044b0021e5a073ccd952463b
Cole Aydelottte <cole.aydelotte@gmail.com>
2024-08-05 04:34:21 -0400

    made the project a Next.js project.
    made the project a Next.js project, also added some backend file structure.

---
+++ app/api/chat/route.js
@@ -0,0 +1,20 @@
+import OpenAI from "openai";
+
+import {NextResponse} from 'next/server'
+export async function POST(req) {
+    const openai = new OpenAI();
+    const data = await req.json()
+
+    const completion = await openai.chat.completions.create({
+        messages: [{"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": "Who won the world series in 2020?"},
+            {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+            {"role": "user", "content": "Where was it played?"}],
+        model: "gpt-4o-mini",
+      });
+    
+      console.log(completion.choices[0]);
+
+    console.log(data)
+    return NextResponse.json({message: 'Hello from the server!'})
+}
