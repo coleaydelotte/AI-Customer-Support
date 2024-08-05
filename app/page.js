0c4253d0d930568b62e467ea9e4cf043841a426c
Francisco <figueroaf@moravian.edu>
2024-08-05 18:44:52 -0400

    added new styling, added page.css for the styling, and added animation using the useffect hook from react, styling with normal JSX

--- app/page.js
+++ app/page.js
@@ -1,52 +1,96 @@
-import { Box, Typography } from '@mui/material';
+"use client";
+
+
+import { Box, Typography, TextField } from '@mui/material';
+import './page.css'
+import { Chat } from "@phosphor-icons/react/dist/ssr/Chat";
+import { useEffect } from 'react';
+import { blue } from '@mui/material/colors';
+
 
 export default function Home() {
+
+  /** Animation When The Program Runs*/
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show');
+            } else {
+                entry.target.classList.remove('show');
+            }
+        });
+    });
+    const hiddenElements = document.querySelectorAll('.hidden');
+    hiddenElements.forEach((element) => {
+        observer.observe(element);
+    });
+
+    return () => {
+        hiddenElements.forEach((element) => {
+            observer.unobserve(element);
+        });
+    };
+}, []); 
+
+
   return (
-    <Box
+
+    <Box 
       width={"100vw"}
       height={"100vh"}
-      bgcolor={"#000000"}
+      bgcolor={"#E5E4E2	"}
       display="flex"
     >
-      {/*CHAT BOX*/}
+      {/*CHAT BOX CONTAINER*/}
       <Box
-        display="flex"
-        justifyContent="center"
+      className="hidden"
         alignItems="center"
         width={"20%"}
         height={"60%"}
-        bgcolor={"#ffffff"}
+        bgcolor={"#FFFFFF	"}
         position={"fixed"}
+        zIndex={0}
         bottom={0}
         right={"1%"}
+        boxShadow={"rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px;"}
         sx={{
           borderTopLeftRadius: "30px",
-          borderTopRightRadius: "30px"
+          borderTopRightRadius: "30px",
+          border: "none"
+
         }}
       >
         {/*HEADER*/}
         <Box
+          bgcolor={"#89CFF0	"}
           position={"absolute"}
           top={0}
           width={"100%"}
           height={"10%"}
-          borderBottom={"#222222 2px solid"}
+          display={"flex"}
+          justifyContent={"center"}
+          textAlign={"center"}
+          alignItems={"center"}
           sx={{
             borderTopLeftRadius: "30px",
-            borderTopRightRadius: "30px"
+            borderTopRightRadius: "30px",
+            border: "none"
           }}
+          zIndex={-1}
+
         >
-          <Typography
-            variant={"h5"}
-            color={"#000000"}
-            textAlign={"center"}
-            paddingTop={"10px"}
-            fontFamily={"Roboto"}
-            fontStyle={"italic"}
-          >
-            Customer Service Representative
-          </Typography>
+          <h4 className='title'>CHAT BOT</h4>
         </Box>
+        
+        
+        <div className='text-chat-container'>
+          <input className='input-label' placeholder='Type Here...' type='text' />
+          <button className='send-btn'>
+            <Chat size={25} color='white' />
+          </button>
+        </div>
       </Box>
     </Box>
   );
