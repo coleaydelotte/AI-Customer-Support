d3040e5ea548715cebd5a4e04ce1e06f16e6bb36
Cole Aydelottte <cole.aydelotte@gmail.com>
2024-08-05 05:10:25 -0400

    Added simple styling to the front end.
    Added simple styling to the front end.

--- app/page.js
+++ app/page.js
@@ -7,15 +7,47 @@ export default function Home() {
       height={"100vh"}
       bgcolor={"#000000"}
       display="flex"
-      justifyContent="center"
-      alignItems="center"
     >
-      <Typography
-        variant="h1"
-        color={"#ffffff"}
+      {/*CHAT BOX*/}
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        width={"20%"}
+        height={"60%"}
+        bgcolor={"#ffffff"}
+        position={"fixed"}
+        bottom={0}
+        right={"1%"}
+        sx={{
+          borderTopLeftRadius: "30px",
+          borderTopRightRadius: "30px"
+        }}
       >
-        Hello world!
-      </Typography>
+        {/*HEADER*/}
+        <Box
+          position={"absolute"}
+          top={0}
+          width={"100%"}
+          height={"10%"}
+          borderBottom={"#222222 2px solid"}
+          sx={{
+            borderTopLeftRadius: "30px",
+            borderTopRightRadius: "30px"
+          }}
+        >
+          <Typography
+            variant={"h5"}
+            color={"#000000"}
+            textAlign={"center"}
+            paddingTop={"10px"}
+            fontFamily={"Roboto"}
+            fontStyle={"italic"}
+          >
+            Customer Service Representative
+          </Typography>
+        </Box>
+      </Box>
     </Box>
   );
 }
